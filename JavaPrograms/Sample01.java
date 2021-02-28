8b2e551ceb4582a7b0894d81aac014904d9bb081
Jatin Ambasana <jatinambasana@gmail.com>
2021-02-28 12:24:02 +0530

    Sample programs added

---
+++ JavaPrograms/Sample01.java
@@ -0,0 +1,9 @@
+class A
+{
+    int i = 1;
+    public static void main(String[] args) {
+        A a1 = new A();
+        a1.i += 10;
+        System.out.println(a1.i);
+    }
+}
---
+++ JavaPrograms/Sample02.java
@@ -0,0 +1,8 @@
+package JavaPrograms;
+
+class B
+{
+    public static void main(String[] args) {
+        System.out.println("This is " + 42 );
+    }
+}
