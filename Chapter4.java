18470f26ee235b780ff335df7a32c6e399ecb0cc
Jatin Ambasana <jatinambasana@gmail.com>
2021-03-13 09:16:20 +0530

    Create Chapter4.java

---
+++ Chapter4.java
@@ -0,0 +1,17 @@
+
+public class Chapter4 {
+    int i;
+    
+    void methodA()
+    {
+        int i;
+    }
+
+    void methodB()
+    {
+        int i;
+    }
+    public static void main(String[] args) {
+        
+    }    
+}
